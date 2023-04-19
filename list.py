72993f9e4f0e33954e63ef72bd771760d06a31d2
tarunmahi <tarunmahendra007@gmail.com>
2023-04-19 16:00:50 +0530

    maps

--- list.py
+++ list.py
@@ -9,3 +9,21 @@ print(length)
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 newlist = [float(num) for num in numbers if num%2==0]
 print(newlist)
+
+#lambda functions
+
+l = [2,4,7,3,14,19]
+
+for num in l:
+    check=lambda num:(num%2)==1
+    print(check(num))
+    
+#lambda function 
+## function name = lambda inputs:outputs
+
+#map functions
+## map(func , *iterables)
+ll = [2,4,7,3,14,19]
+
+lll=list(map(ll*10,ll))
+print(lll)
