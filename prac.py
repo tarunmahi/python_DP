f13011c89327ba463d58bf56f7f77589917dcca3
tarunmahi <tarunmahendra007@gmail.com>
2023-04-19 14:42:46 +0530

    dictionaries

--- prac.py
+++ prac.py
@@ -10,3 +10,24 @@ y.var="mohan"
 
 print(x.var)
 print(y.var)
+
+##init function
+
+class val:
+    
+    def __init__(self,number,name):
+        self.number=number
+        self.name=name
+    
+    def retnumber(self):
+        print("hello mr %s"%self.name)
+        return self.number
+var=val(7,"tarun")
+print(var.retnumber())
+
+#dictionaries 
+pairage={"tarun":20,"mohan":30,"mishra":60}
+
+pairage.pop("mohan")
+for name,age in pairage.items():
+    print("Mr %s has an age of %d"%(name,age))
