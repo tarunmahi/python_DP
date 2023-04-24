43600dd711338ea479a59e9b22d2d45cb81c3f3b
tarunmahi <tarunmahendra007@gmail.com>
2023-04-24 21:19:51 +0530

    guess the number

---
+++ projects/madlabs/guess.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(a):
+    guess_num=random.randint(1,a)
+    num=0
+    while num!=guess_num:
+        num=int(input(f"guess the number between 1 to {a} "))
+        if num<guess_num:
+            print("sorry too low")
+        elif num>guess_num:
+            print("sorry too high")
+    print("yay you are correct")
+    
+y=int(input("enter the range "))
+guess(y)
---
+++ projects/madlabs/madlab.py
@@ -0,0 +1,6 @@
+youtuber=" h"
+
+print("hello mr" +youtuber)
+print("hello mr %s"%youtuber)
+print("hello mr {}".format(youtuber))
+print(f"hello mr {youtuber} ")
