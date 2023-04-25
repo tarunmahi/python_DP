5ddf50a6821d9687e9b1ae85c28e28af9ee32e68
tarunmahi <tarunmahendra007@gmail.com>
2023-04-25 22:01:57 +0530

    addded function guess number

--- projects/madlabs/guess.py
+++ projects/madlabs/guess.py
@@ -11,8 +11,23 @@ def guess(a):
             print("sorry too high")
     print("yay you are correct")
     
-y=int(input("enter the range "))
+#y=int(input("enter the range "))
 #guess(y)
 
-def computer():
+def guessed(num):
+    low=1
+    high=num
+    vari=''
+    while vari!='c':
+        if low!=high:
+            guess=random.randint(low,high)
+        else:
+            guess=low
+        vari=input(f" is the number {guess} too low too high or correct ")
+        if vari=='h':
+            high=guess-1
+        elif vari=='l':
+            low=guess+1
+    print(f"yayy the value {guess} is correct")    
     
+guessed(25)
