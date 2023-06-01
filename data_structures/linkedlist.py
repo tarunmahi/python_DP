4504875030db51893bff874f680614415f73c0a8
tarunmahi <tarunmahendra007@gmail.com>
2023-06-02 00:38:27 +0530

    linked list insert-b

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -1,14 +1,27 @@
-class node:
+class Node:
     def __init__(self,data=None,next=None):
         self.data=data
         self.next=next
-    
-class linkedlist:
+class Linkedlist:
     def __init__(self):
         self.head=None
         
+    def insertb(self,data):
+        node=Node(data,self.head)
+        self.head=node
+    def print(self):
+        if self.head==None:
+            print("empty")
+            return
+        itr=self.head
+        llstr=""
+        while itr:
+            llstr+=str(itr.data)+"-->"
+            itr=itr.next
+        print(llstr)
         
-        
-        
-if __name__=='__main__':
-    pass
+ll=Linkedlist()
+ll.insertb(12)
+ll.insertb(24)
+ll.insertb(48)
+ll.print()
