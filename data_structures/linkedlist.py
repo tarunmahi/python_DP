a82851eda6f82ed74ca6a31d8d49f51f2ffed596
tarunmahi <tarunmahendra007@gmail.com>
2023-06-02 00:44:05 +0530

    linked list insert-e

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -9,6 +9,19 @@ class Linkedlist:
     def insertb(self,data):
         node=Node(data,self.head)
         self.head=node
+        
+    def inserte(self,data):
+        if self.head is None:
+            self.head=Node(data,None)
+            return
+        
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=Node(data,None)
+        
+    
+    
     def print(self):
         if self.head==None:
             print("empty")
@@ -23,5 +36,6 @@ class Linkedlist:
 ll=Linkedlist()
 ll.insertb(12)
 ll.insertb(24)
+ll.inserte(34)
 ll.insertb(48)
 ll.print()
