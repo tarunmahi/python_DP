82349a8968a995c215de43c88d0238dd7720a128
tarunmahi <tarunmahendra007@gmail.com>
2023-06-01 23:44:26 +0530

    added changes

--- prac1.py
+++ prac1.py
@@ -1,11 +1,12 @@
-class Sum:
-    def __init__(self,num1,num2):
+class value:
+    def __init__(self,num1):
         self.num1=num1
-        self.num2=num2
-        
-    def addition(self,variable1):
-        return self.num2+self.num1+variable1
     
-var=Sum(5,10)
-
-print(var.addition(5))
+    def power(self,num2,num3):
+        self.num2=num2
+        self.num3=num3
+        return self.num1+num2+num3
+    
+        
+var=value(5)
+print(var.power(5,5))
