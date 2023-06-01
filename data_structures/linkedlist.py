8c75e7fc337c9484933c990f2ba267cbae97beb6
tarunmahi <tarunmahendra007@gmail.com>
2023-06-02 00:03:37 +0530

    added changes

---
+++ data_structures/linkedlist.py
@@ -0,0 +1,14 @@
+class node:
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+    
+class linkedlist:
+    def __init__(self):
+        self.head=None
+        
+        
+        
+        
+if __name__=='__main__':
+    pass
