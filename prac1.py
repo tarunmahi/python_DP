50f5d0080f1813b2b88d2e340e6c563be6b35b7e
tarunmahi <tarunmahendra007@gmail.com>
2023-06-02 17:47:55 +0530

    linked list remove-index

--- prac1.py
+++ prac1.py
@@ -1,12 +1,64 @@
-class value:
-    def __init__(self,num1):
-        self.num1=num1
-    
-    def power(self,num2,num3):
-        self.num2=num2
-        self.num3=num3
-        return self.num1+num2+num3
-    
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+class Linkedlist:
+    def __init__(self):
+        self.head=None
         
-var=value(5)
-print(var.power(5,5))
+    def insertb(self,data):
+        node=Node(data,self.head)
+        self.head=node
+        
+    def inserte(self,data):
+        if self.head==None:
+            self.head=Node(data,None)
+            return
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=Node(data,None)
+    def print(self):
+        if self.head==None:
+            print("linked list empty")
+            return
+        itr=self.head
+        empstr=""
+        while itr:
+            empstr+=str(itr.data)+"-->"
+            itr=itr.next
+            
+        print(empstr)
+    
+    def getl(self):
+        count=0
+        itr=self.head
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+    
+    def remove(self,index):
+        if index==0:
+            self.head=self.head.next
+            return
+        
+        count=0
+        itr=self.head
+        while itr:
+            if count==index-1:
+                itr.next=itr.next.next
+                break
+            itr=itr.next
+            count+=1
+            
+        
+        
+ll=Linkedlist()
+ll.insertb(12)
+ll.insertb(35)
+ll.inserte(1)
+ll.insertb(2)
+ll.print()
+ll.remove(2)
+ll.print()
