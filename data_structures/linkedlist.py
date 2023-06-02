59ff4f35a167651953539ac9a225da3c440e0ba5
tarunmahi <tarunmahendra007@gmail.com>
2023-06-02 18:35:46 +0530

    added linked list complete

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -2,40 +2,81 @@ class Node:
     def __init__(self,data=None,next=None):
         self.data=data
         self.next=next
+    
 class Linkedlist:
     def __init__(self):
         self.head=None
-        
     def insertb(self,data):
         node=Node(data,self.head)
         self.head=node
-        
     def inserte(self,data):
-        if self.head is None:
-            self.head=Node(data,None)
+        if self.head==None:
+            self.insertb(data)
             return
-        
         itr=self.head
         while itr.next:
             itr=itr.next
         itr.next=Node(data,None)
-        
-    
-    
-    def print(self):
-        if self.head==None:
-            print("empty")
+            
+    def insertat(self,pos,data):
+        if pos<0 or pos>self.getlength():
+            raise Exception("invalid range")
+        if pos==0:
+            self.insertb(data)
             return
-        itr=self.head
-        llstr=""
-        while itr:
-            llstr+=str(itr.data)+"-->"
-            itr=itr.next
-        print(llstr)
         
-ll=Linkedlist()
-ll.insertb(12)
-ll.insertb(24)
-ll.inserte(34)
-ll.insertb(48)
-ll.print()
+        itr=self.head
+        count=0
+        while itr:
+            if count==pos-1:
+                node=Node(data,itr.next)
+                itr.next=node
+            itr=itr.next
+            count+=1
+        
+    def remove(self,pos):
+        if pos<0 or pos>self.getlength():
+            raise Exception("invalid range")
+        if pos==0:
+            self.head=self.head.next
+            return
+        count=0
+        itr=self.head
+        while itr:
+            if count==pos-1:
+                itr.next=itr.next.next
+            itr=itr.next
+            count+=1
+    def getlength(self):
+        count=0
+        
+        itr=self.head
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+    def print(self):
+        
+        if self.head==None:
+            print("empty list")
+        itr=self.head
+        emp_str=""
+        while itr:
+            emp_str+=str(itr.data)+"-->"
+            itr=itr.next
+        print(emp_str)
+
+
+if __name__=="__main__":
+    ll=Linkedlist()
+    ll.insertb(34)
+    ll.insertb(12)
+    ll.inserte(22)
+    ll.inserte(21)
+    ll.print()
+    ll.insertat(3,11)
+    ll.insertat(1,45)
+    ll.print()
+    ll.remove(3)
+    ll.print()
+    ll.getlength()
--- prac1.py
+++ prac1.py
@@ -39,6 +39,8 @@ class Linkedlist:
         return count
     
     def remove(self,index):
+        if index<0 and index>self.getl():
+            raise Exception("index bound out of range")
         if index==0:
             self.head=self.head.next
             return
@@ -51,8 +53,28 @@ class Linkedlist:
                 break
             itr=itr.next
             count+=1
-            
+    
+    def insertat(self,pos,data):
+        if pos<0 and pos>self.getl():
+            raise Exception("position is not in range")
         
+        if pos==0:
+            self.insertb(data)
+            return
+        
+        count=0
+        itr=self.head
+        while itr:
+            if count==pos-1:
+                node=Node(data,itr.next)
+                itr.next=node
+            itr=itr.next
+            count+=1
+    def insertv(self,data_values):
+        for x in data_values:
+            self.inserte(x)
+        ll2=[name for name in data_values if name[0]=="t" or name[0]=="m"]
+        print(ll2)
         
 ll=Linkedlist()
 ll.insertb(12)
@@ -62,3 +84,8 @@ ll.insertb(2)
 ll.print()
 ll.remove(2)
 ll.print()
+ll.insertat(1,25)
+ll.insertat(4,50)
+ll.insertv(["tarun","mahi","mishra","ruchi","siri"])
+ll.print()
+ll.insertv
