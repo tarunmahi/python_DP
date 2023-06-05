0cae2971903b16c9b6525aa04b2e449143b82358
tarunmahi <tarunmahendra007@gmail.com>
2023-06-06 00:44:11 +0530

    added changes

--- pra.py
+++ pra.py
@@ -1,15 +1,5 @@
-def kadanes(seq):
-    curr_sum=0
-    max_sum=0
-    
+def reverse(seq):
     for i in range(len(seq)):
-        curr_sum=max(seq[i],curr_sum+seq[i])
-        max_sum=max(curr_sum,max_sum)
-    return max_sum
-    
-
-
-
-
-arr=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(f" the contigous or max sub array of values {arr} is {kadanes(arr)}")
+        print(seq[::-1])
+val="tarun"
+reverse(val)
