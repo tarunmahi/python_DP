9ca51a7caf705d53e564fd80b9bcf710a3a39eff
tarunmahi <tarunmahendra007@gmail.com>
2023-06-16 12:17:21 +0530

    added sieve of eratosthenes

--- DP/Sieve_of_eratostehenes.py
+++ DP/Sieve_of_eratostehenes.py
@@ -1,19 +1,18 @@
 def sieve(num):
-    check_p=[True]*(num+1)
+    check_p=[True]*(num)
     check_p[0]=check_p[1]=False
     
     p=2
     while p*p<=num:
         if check_p[p]:
-            for i in range(p*p,num+1,p):
+            for i in range(p*p,num,p):
                 check_p[i]=False
         p+=1
-        
     primes=[]
-    for i in range(2,num+1):
+    for i in range(2,num):
         if check_p[i]:
             primes.append(i)
-    return primes 
+    return primes
             
-limit=200
+limit=10
 print(f"the total prime are an the list is {sieve(limit)}")
