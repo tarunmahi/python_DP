b688a9b032ffa54b53d0a99cc5d6469df57416e9
tarunmahi <tarunmahendra007@gmail.com>
2023-06-18 14:36:37 +0530

    added hash function

--- prac1.py
+++ prac1.py
@@ -1,30 +1,52 @@
-def heapify(arr,n,i):
-    largest=i
-    left=2*i+1
-    right=2*i+2
-    
-    if left<n and arr[left]>arr[largest]:
-        largest=left
-    if right<n and arr[right]>arr[largest]:
-        largest=right
-    if largest!=i:
-        arr[i],arr[largest]=arr[largest],arr[i]
-        heapify(arr,n,largest)
+class HashTable:
+    def __init__(self,size):
+        self.size=size
+        self.table=[[] for _ in range(size)]
+    def hash_func(self,key):
+        return hash(key)%self.size
+    def insert(self,key,value):
+        index=self.hash_func(key)
+        bucket=self.table[index]
+        
+        for i in range(len(bucket)):
+            exis_key,exis_val=bucket[i]
+            if key==exis_key:
+                bucket[i]=((key,value))
+        bucket.append((key,value))
+    def get(self,key):
+        index=self.hash_func(key)
+        bucket=self.table[index]
+        
+        for i in range(len(bucket)):
+            exis_k,exis_v=bucket[i]
+            if key==exis_k:
+                return exis_v
+        return None
+    def remove(self,key):
+        index=self.hash_func(key)
+        bucket=self.table[index]
+        
+        for i in range(len(bucket)):
+            exis_key,exis_val=bucket[i]
+            if key==exis_key:
+                del bucket[i]
 
-def heap_sort(arr):
-    n=len(arr)
-    
-    for i in range((n//2)-1,-1,-1):
-        heapify(arr,n,i)
-    for i in range(n-1,0,-1):
-        arr[0],arr[i]=arr[i],arr[0]
-        heapify(arr,i,0)
-    return arr
+    def display(self):
+        for i in range(len(self.table)):
+            bucket=self.table[i]
+            print(f"the value of bucket{i} is {bucket}")
+# Example usage:
+hash_table = HashTable(10)
 
+hash_table.insert('apple', 5)
+hash_table.insert('banana', 7)
+hash_table.insert('orange', 2)
 
+print(hash_table.get('apple'))    # Output: 5
+print(hash_table.get('banana'))   # Output: 7
+print(hash_table.get('orange'))   # Output: 2
+print(hash_table.get('grape'))    # Output: None
 
+hash_table.remove('banana')
 
-
-array = [9, 5, 2, 8, 1, 10]
-sorted_array = heap_sort(array)
-print(sorted_array)
+hash_table.display()
