779dfc0ec2ea1a23c4ce4b8fbf8335a0edce177c
tarun <tarunmahendra007@gmail.com>
2023-06-21 12:46:42 +0530

    bst added

--- data_structures/Trees/bst.py
+++ data_structures/Trees/bst.py
@@ -21,19 +21,45 @@ class BinarySearchTree:
             if not node.right:
                 node.right=Node(value)
             else:
-                self.rec_insert(node.right,value)
+                self.rec_insert(node.right,value) 
     def inorder(self):
-        self.rec_inorder(self.root)
-    def rec_inorder(self,node):
+        self.rec_in(self.root)
+    def rec_in(self,node):
         if node:
-            self.rec_inorder(node.left)
+            self.rec_in(node.left)
             print(node.value,end=" ")
-            self.rec_inorder(node.right)
-    def minimum(self):
-        node=self.root
-        while node.left:
-            node=node.left
-        print(node.value,end='\n')
+            self.rec_in(node.right)
+    def search(self,value):
+        return self.rec_search(self.root,value)
+    def rec_search(self,node,value):
+        if node is None or node.value==value:
+            return node
+        if value<node.value:
+            return self.rec_search(node.left,value)
+        return self.rec_search(node.right,value)
+    def minimum(self,node):
+        current=node
+        while current.left:
+            current=current.left
+        return current
+    def delete(self,value):
+        self.root=self.rec_delete(self.root,value)
+    def rec_delete(self,node,value):
+        if node is None:
+            return node
+        if value<node.value:
+            node.left=self.rec_delete(node.left,value)
+        elif value>node.value:
+            node.right=self.rec_delete(node.right,value)
+        else:
+            if not node.left:
+                return node.right
+            if not node.right:
+                return node.left
+            temp=self.minimum(node.right)
+            node.value=temp.value
+            node.right=self.rec_delete(node.right,temp.value)
+        return node
             
 
 
@@ -50,16 +76,15 @@ bst.insert(80)
 
 # Perform inorder traversal to print the elements in sorted order
 bst.inorder()  # Output: 20 30 40 50 60 70 80
-bst.minimum()
-"""
+
+
 # Search for a value
-result = bst.search(60)
+result = bst.search(20)
 if result:
     print("Found")
 else:
     print("Not found")
 
 # Delete a value
-bst.delete(30)
-bst.inorder_traversal()
-"""
+bst.delete(70)
+bst.inorder()
