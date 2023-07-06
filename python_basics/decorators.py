bd036055c25b1dae5aba707a51bfccf83be9bb34
tarun <tarunmahendra007@gmail.com>
2023-07-06 15:16:28 +0530

    added decorators

--- python_basics/decorators.py
+++ python_basics/decorators.py
@@ -1,56 +1,13 @@
-
-# defining a decorator
-def hello_decorator(func):
-
-	# inner1 is a Wrapper function in
-	# which the argument is called
-	
-	# inner function can access the outer local
-	# functions like in this case "func"
-	def inner1():
-		print("Hello, this is before function execution")
-
-		# calling the actual function now
-		# inside the wrapper function.
-		func()
-
-		print("This is after function execution")
-		
-	return inner1
-
-
-# defining a function be called inside wrapper
-def function_to_be_used():
-	print("This is inside the function !!")
-
-
-# passing 'function_to_be_used' inside the
-# decorator to control its behaviour
-function_to_be_used = hello_decorator(function_to_be_used)
-
-
-# calling the function
-function_to_be_used()
-
-def make_pretty(func):
-    # define the inner function 
-    def inner():
-        # add some additional behavior to decorated function
-        print("I got decorated")
-
-        # call original function
+def called(func):
+    def triple():
+        print("hello")
         func()
-    # return the inner function
-    return inner
+    return triple
+        
+        
+def square():
+    print("my god")
 
-# define ordinary function
-def ordinary():
-    print("I am ordinary")
-    
-# decorate the ordinary function
-decorated_func = make_pretty(ordinary)
-
-# call the decorated function
-decorated_func()
-#main reasonn
+val=called(square)
+val()
 
