a4f5f19704654b818a6904e8dec2ba7aac42eccf
tarun <tarunmahendra007@gmail.com>
2023-07-07 21:19:17 +0530

    added oops complete

--- OOPS/abstraction.py
+++ OOPS/abstraction.py
@@ -2,3 +2,23 @@
 Abstraction: Abstraction is the process of representing complex real-world entities as simplified models within a program. 
 It involves hiding the unnecessary details and exposing only the essential features and behaviors.
 """
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
+
+dog = Dog()
+cat = Cat()
+
+print(dog.make_sound())   # Output: Woof!
+print(cat.make_sound())   # Output: Meow!
--- OOPS/polymorphism.py
+++ OOPS/polymorphism.py
@@ -2,3 +2,19 @@
 Polymorphism: Polymorphism refers to the ability of objects of different classes to respond to the same method or attribute in different ways. 
 It allows different objects to be treated uniformly based on their common characteristics or interfaces.
 """
+class Dog:
+    def sound(self):
+        print("bark")
+class cat:
+    def sound(self):
+        print("meow")
+class cow:
+    def sound(self):
+        print("moooohhhh")
+        
+a=Dog()
+b=cat()
+c=cow()
+a.sound()
+b.sound()
+c.sound()
