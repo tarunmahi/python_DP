d06db1c38e4e00a93dbef654655a22007ff31b50
tarun <tarunmahendra007@gmail.com>
2023-07-29 10:38:38 +0530

    practice messages

--- prac.py
+++ prac.py
@@ -1,9 +1,62 @@
-arr="tarun"
+"""
+def selection_sort(arr):
+    n=len(arr)
+    
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
 
-l=[]
-l2=[]
-for i in range(len(arr)):
-    l.append(arr[i])
-for j  in range(len(l)):
-    l2.append(l.pop())
-print(l2)
+
+def bubble_sort(arr):
+    n=len(arr)
+    
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr=[5,4,3,1,2,7,6,5,34,12,98,65,38]
+
+def insert_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+
+class heap:
+    def heapify(self,arr,n,i):
+        largest=i
+        left=2*i+1
+        right=2*i+2
+        if left<n and arr[left]>arr[largest]:
+            largest=left
+        if right<n and arr[right]>arr[largest]:
+            largest=right
+        if largest!=i:
+            arr[i],arr[largest]=arr[largest],arr[i]
+            self.heapify(arr,n,largest)
+    
+    def heap_sort(self,arr):
+        n=len(arr)
+        for i in range((n//2)-1,-1,-1):
+            self.heapify(arr,n,i)
+        for i in range(n-1,0,-1):
+            arr[0],arr[i]=arr[i],arr[0]
+            self.heapify(arr,i,0)
+        return arr
+        
+"""
+
+arr=[5,4,3,1,2,7,6,5,34,12,98,65,38]
+
+val=heap()
+print(val.heap_sort(arr))
