0eeec4d082a5890d8b029999f1e5ba7fda7bf948
tarun <tarunmahendra007@gmail.com>
2023-10-11 22:09:36 +0530

    added longes common subsequence

--- data_structures/sorting/selection.py
+++ data_structures/sorting/selection.py
@@ -1,7 +1,7 @@
 """
 space complexity:- o(n^2)
 time complexity:- o(1)
-"""
+
 def selection_sort(arr):
     n=len(arr)
     for i in range(n):
@@ -11,7 +11,20 @@ def selection_sort(arr):
                 min_index=j
         arr[min_index],arr[i]=arr[i],arr[min_index]
     return arr
+"""
 
+
+
+
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[min_index],arr[i]=arr[i],arr[min_index]
+    return arr
 arr1=[5,4,3,1,2,7,6,56]
 
 print(selection_sort(arr1))
