c1441593b7cc047045fa9b59e35cb4a8b84796bf
tarun <tarunmahendra007@gmail.com>
2024-04-30 17:44:36 +0530

    merge sort easy

--- data_structures/sorting/prac.py
+++ data_structures/sorting/prac.py
@@ -1,74 +1,33 @@
-class Node:
-    def __init__(self,data=None,next=None):
-        self.data=data
-        self.next=next
-class LL:
-    def __init__(self):
-        self.head=None
-    def insert_bi(self,data):
-        node=Node(data,self.head)
-        self.head=node
-    def insert_end(self,data):
-        if self.head==None:
-            self.insert_bi(data)
-            return
-        itr=self.head
-        while itr.next:
-            itr=itr.next
-        itr.next=Node(data,None)
+def Merge_sort(A):
+    if len(A)>1:
         
-    def insert_pos(self,data,pos):
-        if pos<0 and pos>self.getlen():
-            raise Exception("invalid range")
-        if pos==0:
-            self.insert_bi(data)
-            return
-        else:
-            count=0
-            itr=self.head
-            while itr:
-                if count==pos-1:
-                    node=Node(data,itr.next)
-                    itr.next=node
-                itr=itr.next
-                count+=1
-                
-                
-    def remove(self,pos):
-        if pos<0 and pos>self.getlen():
-            raise Exception("invalid range")
-        count=0
-        itr=self.head
-        while itr:
-            if count==pos-1:
-                itr.next=itr.next.next
-            itr=itr.next
-            count+=1
-    def getlen(self):
-        count=0
-        itr=self.head
-        while itr:
-            count+=1
-            itr=itr.next
-        return count
-    def print(self):
-        itr=self.head
-        emp=""
-        while itr:
-            emp+=str(itr.data)+"-->"
-            itr=itr.next
-        print(emp)
-    
-    
-    
-ll=LL()
-ll.insert_bi(24)
-ll.insert_bi(12)
-ll.insert_end(90)
-ll.insert_bi(5)
-ll.insert_end(13)
-ll.print()
-ll.remove(3)
-ll.insert_pos(50,3)
-ll.print()
-print(ll.getlen())
+        mid=len(A)//2
+        lefth=A[:mid]
+        righth=A[mid:]
+        Merge_sort(lefth)
+        Merge_sort(righth)
+        i=j=k=0
+        while i<len(lefth) and j<len(righth):
+            if lefth[i]<righth[i]:
+                A[k]=lefth[i]
+                i=i+1
+            else:
+                A[k]=righth[j]
+                j=j+1
+            k=k+1
+        while i<len(lefth):
+            A[k]=lefth[i]
+            i=i+1
+            k=k+1
+        while j<len(righth):
+            A[k]=righth[j]
+            j=j+1
+            k=k+1
+        return A
+
+arr=[9,8,7,6,5,4,3,2,1]
+Merge_sort(arr)
+print(arr)
+        
+        
+            
--- prac1.py
+++ prac1.py
@@ -1,19 +1,110 @@
-def quick_sort(arr,low,high):
-    if low<high:
-        partiton_index=partition(arr,low,high)
+class Node:
+    def __init__(self,data,next=None,prev=None):
+        self.data=data
+        self.next=next
+        self.prev=prev
+    
+class DoublyLinkedList:
+    def __init__(self):
+        self.head=None
+    def insertAtStart(self,data):
+        if self.head==None:
+            self.head=Node(data,self.head)
+        else:
+            node=Node(data,self.head)
+            self.head.prev=node
+            self.head=node
+    def insertAtEnd(self,data):
+        if self.head==None:
+            self.insertAtStart(data)
+            return
+        ctr=self.head
+        while ctr.next:
+            ctr=ctr.next
+        node=Node(data,None,ctr)
+        ctr.next=node
+    def delete(self,pos):
+        if pos==0:
+            self.head=self.head.next
+        if pos<0 or pos>self.getlen():
+            raise Exception("invalid range")
+        ctr=self.head
+        count=0
+        while ctr:
+            if count==pos-1:
+                node=ctr.next.next
+                node.prev=node.prev.prev
+                ctr.next=ctr.next.next
+            count+=1
+            ctr=ctr.next
+    def insertatval(self,pos,data):
+        ctr=self.head
+        count=0
+        if pos==0:
+            self.insertAtStart(data)
+        if pos<0 or pos>self.getlen():
+            raise Exception("invalid range given")
+        while ctr:
+            if count==pos-1:
+                node=Node(data,ctr.next,ctr)
+                ctr.next.prev=node
+                ctr.next=node
+            count+=1
+            ctr=ctr.next
         
-        quick_sort(arr,low,partiton_index-1)
-        quick_sort(arr,partiton_index+1,high)
-def partition(arr,low,high):
-    pivot=arr[high]
-    i=low-1
-    for j in range(low,high):
-        if pivot>=arr[j]:
-            i+=1
-            arr[i],arr[j]=arr[j],arr[i]
-    arr[i+1],arr[high]=arr[high],arr[i+1]
-    return i+1
-
-array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-quick_sort(array, 0, len(array) - 1)
-print(array)
+    def search(self,data):
+        ctr=self.head
+        count=0
+        while ctr:
+            if ctr.data==data:
+                print(f"the value {data} found at pos {count}")
+            ctr=ctr.next
+            count+=1
+    def getlen(self):
+        count=0
+        ctr=self.head
+        while ctr:
+            count+=1
+            ctr=ctr.next
+        return count        
+    def printdll(self):
+        ctr=self.head
+        emp=""
+        while ctr:
+            emp+=str(ctr.data)+"-->"
+            ctr=ctr.next
+        print(emp)
+    def revprint(self):
+        ctr=self.head
+        emp=""
+        while ctr.next:
+            ctr=ctr.next
+        while ctr:
+            emp+=str(ctr.data)+"-->"
+            ctr=ctr.prev
+        print(emp)
+if __name__=='__main__':    
+    dll = DoublyLinkedList()
+    dll.insertAtStart(1)
+    dll.insertAtStart(2)
+    dll.insertAtEnd(34)
+    dll.insertAtEnd(87)
+    dll.insertAtStart(45)
+    dll.printdll()
+    dll.revprint()
+    #dll.delete(3)
+    #dll.delete(1)
+    dll.delete(2)
+    
+    dll.insertatval(1,40)
+    dll.insertatval(1,15)
+    dll.insertatval(0,100)
+    """
+    dll.insertatval(0,12)
+    dll.insertAtEnd(5)
+    dll.search(100)
+    dll.search(350)
+   """
+    dll.search(100)
+    dll.printdll()
+    dll.revprint()
